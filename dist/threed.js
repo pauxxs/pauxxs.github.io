a3a57a6d27965cc9b13ff87e3353f6e00f27ba5e
pauxxs <91545867+pauxxs@users.noreply.github.com>
2022-11-10 15:01:36 -0500

    A random pick

--- dist/threed.js
+++ dist/threed.js
@@ -1,4 +1,5 @@
 import * as THREE from "../_snowpack/pkg/three.js";
+const images = ["dickson.jpg", "dude.jpg"];
 export default function threeDInit() {
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1e3);
@@ -11,7 +12,8 @@ export default function threeDInit() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
   const geometry = new THREE.SphereGeometry(0.5, 100, 100);
-  const texture = new THREE.TextureLoader().load("./res/dude.jpg");
+  const image = "./res/" + images[Math.floor(Math.random()*images.length)];
+  const texture = new THREE.TextureLoader().load(image);
   const material = new THREE.MeshPhongMaterial({map: texture});
   const sphere = new THREE.Mesh(geometry, material);
   scene.add(sphere);
